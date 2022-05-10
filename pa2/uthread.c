672807b22ab9286b0ea205232ee34dc6dea3d971
woojin <dnwls813@ajou.ac.kr>
2022-05-11 04:32:30 +0900

    Implement FIFO Scheduler

--- pa2/uthread.c
+++ pa2/uthread.c
@@ -7,7 +7,10 @@
 #include "list_head.h"
 #include "types.h"
 
+#define MEM 3000
+
 /* You can include other header file, if you want. */
+
 
 /*******************************************************************
  * struct tcb
@@ -20,12 +23,66 @@
 struct tcb {
     struct list_head list;
     ucontext_t *context;
-    enum uthread_state state;
+    enum uthread_state state; //ready, running, terminated
     int tid;
 
     int lifetime; // This for preemptive scheduling
     int priority; // This for priority scheduling
 };
+
+int n_tcbs = 0;
+struct ucontext_t *t_context;
+LIST_HEAD(tcbs);
+
+struct tcb* Main;
+
+struct tcb* prev_tcb;
+struct tcb* running_tcb;
+struct tcb* daum_tcb;
+
+int terminate_tid = 0;
+int current_tid = 0;
+int next_tid = 0;
+
+bool start = false;
+bool finish = false;
+enum uthread_sched_policy current_policy;
+
+void push_tcbs(ucontext_t* new_context, int params[]) // tcbs에 tcb저장
+{
+    struct tcb* new_tcb = (struct tcb*)malloc(sizeof(struct tcb));
+    
+    new_tcb->context = new_context;
+    new_tcb->state = READY;
+    new_tcb->tid = params[0];
+    new_tcb->lifetime = params[1];
+    new_tcb->priority = params[2];
+
+    list_add_tail(&(new_tcb->list), &tcbs);
+    n_tcbs++;
+}
+
+void pop_tcbs(int del_tid)
+{
+		/* TODO: Implement this function */
+		struct list_head* tp;
+		struct tcb* del_tcb;
+    struct tcb* tp_tcb;
+
+		list_for_each(tp, &tcbs){
+      tp_tcb = list_entry(tp, struct tcb, list);
+      if(tp_tcb->tid == del_tid){
+			  del_tcb = list_entry(tp, struct tcb, list);
+        break;
+      }
+		}
+
+    n_tcbs--;
+    if(n_tcbs==0)
+      start=false;
+		list_del(&(del_tcb->list));
+		free(del_tcb);
+}
 
 /***************************************************************************************
  * LIST_HEAD(tcbs);
@@ -34,11 +91,6 @@ struct tcb {
  *    Initialize list head of thread control block. 
  *
  **************************************************************************************/
-LIST_HEAD(tcbs);
-
-int n_tcbs = 0;
-
-struct ucontext_t *t_context;
 
 /***************************************************************************************
  * next_tcb()
@@ -50,22 +102,78 @@ struct ucontext_t *t_context;
  **************************************************************************************/
 void next_tcb() {
 
-    /* TODO: You have to implement this function. */
+  struct list_head* tp;
+  struct tcb* current_tcb;
 
+  /* TODO: You have to implement this function. */
+  if(current_policy == FIFO){
+
+    daum_tcb = fifo_scheduling(Main);
+
+    if(daum_tcb != NULL){
+      
+      terminate_tid = daum_tcb->tid;
+      __exit();
+      __initialize_exit_context();
+
+     printf("SWAP %d -> %d\n", running_tcb->tid, daum_tcb->tid);
+      //fprintf(stderr, "SWAP %d -> %d\n", running_tcb->tid, daum_tcb->tid);
+
+      prev_tcb = running_tcb;
+      running_tcb = daum_tcb;
+
+      makecontext(running_tcb->context, (void*)&next_tcb, 0);
+      swapcontext(prev_tcb->context, running_tcb->context);
+
+      return;
+    }
+    else{
+      finish=true;
+      printf("SWAP %d -> %d\n", running_tcb->tid, -1);
+      //fprintf(stderr, "SWAP %d -> %d\n", running_tcb->tid, -1);
+      running_tcb = Main;
+      return;
+    }
+    
+  }
+  else if(current_policy == SJF){
+
+  }
+  else if(current_policy == RR){
+
+  }
+  else{
+
+  }
 }
 
-/***************************************************************************************
- * struct tcb *fifo_scheduling(struct tcb *next)
- *
- * DESCRIPTION
- *
- *    This function returns a tcb pointer using First-In-First-Out policy
- *
- **************************************************************************************/
+void finish_check(){
+  struct list_head* tp;
+  struct tcb* current_tcb;
+
+  list_for_each(tp, &tcbs) {
+    current_tcb = list_entry(tp, struct tcb, list);
+    if(current_tcb->state != TERMINATED){
+      finish = false;
+      return;
+    }
+  }
+  finish = true;
+  return;
+}
+
 struct tcb *fifo_scheduling(struct tcb *next) {
+  struct list_head* tp;
+  struct tcb* current_tcb;
+  bool flag = false;
 
-    /* TODO: You have to implement this function. */
-
+  list_for_each(tp, &tcbs) {
+    current_tcb = list_entry(tp, struct tcb, list);
+    //printf("current_tcb: %d\n",current_tcb->tid);
+    if(current_tcb->state == READY)
+      return current_tcb;
+  }
+  return NULL;
 }
 
 /***************************************************************************************
@@ -114,15 +222,28 @@ struct tcb *sjf_scheduling(struct tcb *next) {
  * uthread_init(enum uthread_sched_policy policy)
  *
  * DESCRIPTION
- *    Initialize main tread control block, and do something other to schedule tcbs
+
+ *    Initialize main thread control block, and do something other to schedule tcbs
  *
  **************************************************************************************/
+
 void uthread_init(enum uthread_sched_policy policy) {
-    
     /* TODO: You have to implement this function. */
+    current_policy = policy;
 
+    Main = (struct tcb*)malloc(sizeof(struct tcb));
+    ucontext_t* new_context = (ucontext_t*)malloc(sizeof(ucontext_t));
+    
+    Main->context = new_context;
+    Main->state = READY;
+    Main->tid = -1;
+    Main->lifetime = 0;
+    Main->priority = 0;
+
+    getcontext(Main->context);
+
+    running_tcb = Main;
 }
-
 
 /***************************************************************************************
  * uthread_create(void* stub(void *), void* args)
@@ -134,8 +255,19 @@ void uthread_init(enum uthread_sched_policy policy) {
  **************************************************************************************/
 int uthread_create(void* stub(void *), void* args) {
 
-    /* TODO: You have to implement this function. */
+  /* TODO: You have to implement this function. */
+  int* params = (int*)args;
+  ucontext_t* ncontext = (ucontext_t*)malloc(sizeof(ucontext_t));
+  getcontext(ncontext);
 
+  char* new_stack = (char*)malloc(sizeof(char)*3000);
+
+  ncontext->uc_link=0;
+  ncontext->uc_stack.ss_sp=malloc(MEM);
+  ncontext->uc_stack.ss_size=MEM;
+  ncontext->uc_stack.ss_flags=0;
+
+  push_tcbs(ncontext, params);
 }
 
 /***************************************************************************************
@@ -149,7 +281,24 @@ int uthread_create(void* stub(void *), void* args) {
 void uthread_join(int tid) {
 
     /* TODO: You have to implement this function. */
+    struct list_head* tp;
+    struct tcb* current_tcb;
 
+    list_for_each(tp, &tcbs) {
+      current_tcb = list_entry(tp, struct tcb, list);
+
+      if(current_tcb->tid == tid){
+        finish_check();
+        while(!finish){
+          next_tcb();
+        }
+        printf("JOIN %d\n", tid);
+        //fprintf(stderr, "JOIN %d\n", tid);
+        pop_tcbs(tid);
+        break;
+      }
+
+    }
 }
 
 /***************************************************************************************
@@ -161,9 +310,19 @@ void uthread_join(int tid) {
  *
  **************************************************************************************/
 void __exit() {
+  /* TODO: You have to implement this function. */
+  struct list_head* tp;
+  struct tcb* current_tcb;
 
-    /* TODO: You have to implement this function. */
+  list_for_each(tp, &tcbs) {
+    current_tcb = list_entry(tp, struct tcb, list);
 
+    if(current_tcb->tid == terminate_tid){
+      current_tcb->state = TERMINATED;
+      break;
+    }
+
+  }
 }
 
 /***************************************************************************************
@@ -177,7 +336,7 @@ void __exit() {
 void __initialize_exit_context() {
 
     /* TODO: You have to implement this function. */
-
+  (daum_tcb->context)->uc_link = running_tcb->context;
 }
 
 /***************************************************************************************
